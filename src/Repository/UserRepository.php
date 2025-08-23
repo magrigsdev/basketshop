23f6420e6fe0f8594c96f1ba9fad2e364a81514a
Eliandre <96908551+magrigsdev@users.noreply.github.com>
2025-08-23 02:46:37 +0200

    test insert data user  done

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -2,9 +2,11 @@
 
 namespace App\Repository;
 
+use Dom\Entity;
 use App\Entity\User;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<User>
@@ -15,29 +17,28 @@ class UserRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, User::class);
     }
+    public function save(User $user, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($user);       
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }   
+    }
+    public function EmailIsExist(string $email):bool
+    {
+        $user = $this->findOneBy(['email'=>$email]);
+        if($user){
+            return true;
+        }
+        return false;
+    }
+    public function remove(User $user, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($user);       
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
 
-    //    /**
-    //     * @return User[] Returns an array of User objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?User
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+   
 }
---
+++ tests/Repository/UseRepositoryTest.php
@@ -0,0 +1,40 @@
+<?php 
+namespace App\Tests\Repository;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase; 
+
+class UseRepositoryTest extends KernelTestCase
+{
+    private UserRepository $user_repository;
+    protected function setUp(): void
+    {
+        self::bootKernel();
+        $this->user_repository = self::getContainer()->get(UserRepository::class);
+    }
+    public function testInsertUser(): void
+    {
+        $user = new User();
+        $user->setEmail('test2@example.com');
+        $user->setPassword('password123'); 
+        $user->setRoles('ROLE_USER');
+        $user->setFirstName('John');
+        $user->setLastName('Doe');
+        $user->setAddress('14 Rue de la Paix');
+        $user->setPostalCode('75002');
+        $user->setCity('Paris');
+
+        if($this->user_repository->EmailIsExist($user->getEmail()))
+        {
+            $this->assertNotNull($user->getId(),
+            'User with this email already exists.');     
+        }
+        else{
+            $this->user_repository->save($user, true);
+            $this->assertNotNull($user->getId());
+        }    
+    }
+
+
+}
