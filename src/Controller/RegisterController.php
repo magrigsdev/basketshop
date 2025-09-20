9970ae34fbe09a6e58207696b20bcb60a5780acb
Eliandre <96908551+magrigsdev@users.noreply.github.com>
2025-09-20 12:44:00 +0200

    clean code

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,16 +2,9 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManager;
-use App\Entity\User;
-use App\Repository\UserRepository;
-use App\Service\UserService;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpFoundation\Request;
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 
